2bdf68674b2d44495480320324823f0fa744270e
Leonardo Tolosa <tolosa@gmail.com>
2025-01-16 12:49:47 -0300

    Add grouping of shop items by section

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
   Grid,
   Card,
   CardMedia,
@@ -8,6 +11,8 @@ import {
   Typography,
   CircularProgress,
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { groupBy } from "lodash";
 
 const ShopList = () => {
   const [items, setItems] = useState([]);
@@ -33,35 +38,48 @@ const ShopList = () => {
     fetchItems();
   }, []);
 
+  const groupedItems = groupBy(items, (i) => i.section.name); // name, id, category
+
   return (
     <>
       {loading ? (
         <CircularProgress />
       ) : (
-        <Grid container spacing={4}>
-          {items.map((item) => (
-            <Grid item key={item.mainId} xs={12} sm={4} md={3}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  image={
-                    item.displayAssets[0].background ||
-                    item.displayAssets[0].url
-                  }
-                  alt={item.displayName}
-                />
-                <CardContent>
-                  <Typography variant="h6" component="div">
-                    {item.displayName}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.price.regularPrice} V-Bucks
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+        <>
+          {Object.entries(groupedItems).map(([sectionName, sectionItems]) => (
+            <Accordion defaultExpanded key={sectionName}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>{sectionName}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container spacing={3}>
+                  {sectionItems.map((item) => (
+                    <Grid item key={item.mainId} xs={12} sm={4} md={3}>
+                      <Card>
+                        <CardMedia
+                          component="img"
+                          image={
+                            item.displayAssets[0].background ||
+                            item.displayAssets[0].url
+                          }
+                          alt={item.displayName}
+                        />
+                        <CardContent>
+                          <Typography variant="h6" component="div">
+                            {item.displayName}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {item.price.regularPrice} V-Bucks
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
           ))}
-        </Grid>
+        </>
       )}
     </>
   );
