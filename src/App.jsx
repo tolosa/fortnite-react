79a98a13f419d0bb2415aca6824beb91149c0d01
Leonardo Tolosa <tolosa@gmail.com>
2025-01-16 17:07:44 -0300

    Add theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { Container, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
 import { ShopList } from "./components/ShopList";
 
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
+
 const App = () => (
-  <Container sx={{ marginTop: 4 }}>
-    <Typography variant="h4" gutterBottom align="center">
-      Fortnite Daily Shop
-    </Typography>
-    <ShopList />
-  </Container>
+  <>
+    <ThemeProvider theme={theme} defaultMode="dark" noSsr>
+      <Container
+        sx={{
+          marginTop: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Typography variant="h4" gutterBottom align="center">
+          Fortnite Daily Shop
+        </Typography>
+        <ShopList />
+      </Container>
+    </ThemeProvider>
+  </>
 );
 
 export default App;
