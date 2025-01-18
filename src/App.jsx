a1edcb1ea6eb9bb998e3a009e70338b911ac1370
Leonardo Tolosa <tolosa@gmail.com>
2025-01-18 18:48:20 -0300

    Fixed global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Container, Typography } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { ShopList } from "./components/ShopList";
 
 const theme = createTheme({
-  colorSchemes: {
-    dark: true,
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#242424",
+    },
   },
 });
 
 const App = () => (
   <ThemeProvider theme={theme} noSsr>
+    <CssBaseline />
     <Container sx={{ maxWidth: "lg", mx: "auto" }}>
       <Typography variant="h4" sx={{ my: 4, ml: "10px" }}>
         Fortnite Daily Shop
